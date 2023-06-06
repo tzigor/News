b5654d0163128617086634a965aa08b4170ae082
igortz <igortz@mail.ru>
2023-06-06 10:30:29 +0700

    Lesson 1 task done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}', function (string $name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::get('/news', function () {
+    return 'Apple представила на WWDC2023 свою самую обсуждаемую новинку — AR/VR-гарнитуру, получившую название Apple Vision Pro. Устройство управляется глазами, руками и голосом, будет стоить $3499 и появится в продаже в 2024 году. ';
+});
